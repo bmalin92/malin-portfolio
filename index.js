f79e5f9bdf3c43431c7394a5914045249b248f0c
Benjamin Malin <benjaminamalin@gmail.com>
2017-12-29 22:23:16 -0500

    added new icons and completed bio and prior experience formatting

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict';
 
-$('.bio h2').click(function () {
+$('.bio').click(function () {
   $('.about').show(200);
   $('.bio .bounce').removeClass('bounce');
+  $('.bio-arrow').hide();
 });
 
 $('.experience h2').click(function () {
   $('.jobs').show(200);
   $('.experience .bounce').removeClass('bounce');
+  $('.experience-arrow').hide();
 });
